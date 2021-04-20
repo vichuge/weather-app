e46c1dfb2beb21018c8d26f796bf93bd04b9abf8
V <victor.hugo.pacheco.flores@gmail.com>
2021-04-20 15:39:01 -0500

    Show info on web page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import './style.css';
-import _ from 'lodash';
-
-function component() {
-    const p = document.getElementById('p');
-    p.innerHTML = 'hello there';
-    getWeather();
-}
 
 async function getWeather() {
-    // c6f720738d4ba0af3722fd5e0551d616
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=c6f720738d4ba0af3722fd5e0551d616', { mode: 'cors' });
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=c6f720738d4ba0af3722fd5e0551d616&units=metric');
     const wData = await response.json();
     console.log(wData);
+    console.log(wData.name);
+    console.log(wData.main.temp);
+
+    const myCity = document.getElementById('myCity');
+    myCity.innerHTML = wData.name;
+    const w = document.getElementById('w');
+    w.innerHTML = `${wData.main.temp} °C`;
 }
 
-component();
+getWeather();
