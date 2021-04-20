a23d2f11d9968c35afe640f198e6fc6833216885
V <victor.hugo.pacheco.flores@gmail.com>
2021-04-20 16:17:20 -0500

    Add form to change city

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import './style.css';
 
-async function getWeather() {
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=c6f720738d4ba0af3722fd5e0551d616&units=metric');
+const component = () => {
+    getWeather();
+    const btnModal = document.getElementById('findWeather');
+    btnModal.addEventListener('click', () => {changeCity();});
+    const openModal = document.getElementById('openModal');
+    openModal.addEventListener('click', emptyVals);
+}
+
+const closeModal = () => {
+    document.getElementById('closeModal').click();
+}
+
+const emptyVals = () => {
+    const errors = document.getElementById('error');
+    errors.innerHTML = '';
+    document.getElementById('city').value = '';
+}
+
+const changeCity = () => {
+    const city = document.getElementById('city').value;
+    if (city === '') {
+        const errors = document.getElementById('error');
+        errors.innerHTML = 'City can\'t be blank';
+        return;
+    }
+    console.log(city);
+    getWeather(city);
+    closeModal();
+}
+
+async function getWeather(city = 'London') {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c6f720738d4ba0af3722fd5e0551d616&units=metric`);
     const wData = await response.json();
     console.log(wData);
     console.log(wData.name);
@@ -13,4 +43,4 @@ async function getWeather() {
     w.innerHTML = `${wData.main.temp} °C`;
 }
 
-getWeather();
+component();
