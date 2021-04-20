441e741b89d937120f63dbc951412b6d3e9562a2
V <victor.hugo.pacheco.flores@gmail.com>
2021-04-20 15:11:17 -0500

    Test rick and Morty API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import _ from 'lodash';
 function component() {
     const p = document.getElementById('p');
     p.innerHTML = 'hello there';
+    console.log('hihi!');
+    // https://rickandmortyapi.com/api/character/22
+    getRick();
+}
+
+async function getRick() {
+    const response = await fetch('https://rickandmortyapi.com/api/character/22', {mode: 'cors'});
+    const rickData = await response.json();
+    console.log(rickData);
 }
 
 component();
