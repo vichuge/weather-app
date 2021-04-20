79d7f5e8e1f47528242e9073b1931cce94a632cd
V <victor.hugo.pacheco.flores@gmail.com>
2021-04-20 15:18:56 -0500

    Test weather API

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import _ from 'lodash';
 function component() {
     const p = document.getElementById('p');
     p.innerHTML = 'hello there';
-    console.log('hihi!');
-    // https://rickandmortyapi.com/api/character/22
-    getRick();
+    getWeather();
 }
 
-async function getRick() {
-    const response = await fetch('https://rickandmortyapi.com/api/character/22', {mode: 'cors'});
-    const rickData = await response.json();
-    console.log(rickData);
+async function getWeather() {
+    // c6f720738d4ba0af3722fd5e0551d616
+    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=c6f720738d4ba0af3722fd5e0551d616', { mode: 'cors' });
+    const wData = await response.json();
+    console.log(wData);
 }
 
 component();
